525dee93a6497bc3b45b1ee29c9f38b7a7114aa1
SumanJK <96579301+SumanJK@users.noreply.github.com>
2022-07-10 22:50:30 +0530

    updated posts controller

--- superFam_backend/controllers/postController.js
+++ superFam_backend/controllers/postController.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router= express.Router();
 
 const Post= require('../models/postModel')
+const User= require('../models/userModel')
 
 //CRUD operations
 
@@ -54,6 +55,65 @@ router.delete("/:id", async (req, res)=>{
 
     }catch(err){
         return res.status(500).send(err)
+    }
+})
+
+// Like & dislike the post 
+router.put("/:id/like", async(req, res)=>{
+    try{
+        const post = await Post.findById(req.params.id);
+
+        if(!post.likes.includes(req.body.userId)){
+            await post.updateOne({$push:{likes: req.body.userId}});
+            return res.status(200).send("The post has been liked")
+        }else{
+            await post.updateOne({$pull:{likes: req.body.userId}});
+            return res.status(200).send("The post has been disliked")
+        }
+    }catch(err){
+        return res.status(500).send(err)
+    }
+})
+
+//get a post
+router.get('/:id', async (req, res) => {
+    try{
+        const post = await Post.findById(req.params.id);
+        return res.status(200).send(post)
+    }catch(err){
+        return res.status(500).send(err);
+    }
+})
+
+//get timeline posts 
+router.get('/timeline/all', async (req, res) =>{
+    try{
+        const currUser = await User.findById(req.body.userId);
+        console.log(currUser);
+        const userPosts = await Post.find({userId: currUser._id});
+        const followersPost = await Promise.all(
+            currUser.following.map((friendsId)=>{
+                return Post.find({userId: friendsId});
+            })
+        )
+        // res.json(userPosts.concat(...followersPost));
+        return res.status(200).send(followersPost);
+        
+    }catch(err){
+        return res.status(500).send(err);
+    }
+})
+
+//get all posts of a user 
+router.get('/profile/userposts', async (req, res) =>{
+    try{
+        const currentUser = await User.findById(req.body.userId);
+        const userEveryPosts = await Post.find({userId: currentUser._id});
+        // res.json(userPosts.concat(...followersPost));
+        return res.status(200).send(userEveryPosts);
+        
+    }catch(err){
+        return res.status(500).send(err);
     }
 })
 
