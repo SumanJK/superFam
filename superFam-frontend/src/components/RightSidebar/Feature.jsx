d85307c893c60609cd30d291686ab6d383ae2e32
SumanJK <96579301+SumanJK@users.noreply.github.com>
2022-07-18 14:02:03 +0530

    updated spline animation

--- superFam-frontend/src/components/RightSidebar/Feature.jsx
+++ superFam-frontend/src/components/RightSidebar/Feature.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { chakra, Box, Flex } from "@chakra-ui/react";
-
+import { chakra, Box, Flex, Image } from "@chakra-ui/react";
+import logoPng from "../../assets/default.png"
+import Spline from '@splinetool/react-spline';
 const Feature=()=>{
   return (
     <Flex
@@ -18,25 +19,25 @@ const Feature=()=>{
         w="sm"
         mx="auto"
       >
-        <Box
+        <Flex
           bg="gray.300"
           h={48}
           w="64"
           rounded="lg"
           shadow="md"
-          bgSize="cover"
-          bgPos="center"
-          style={{
-            backgroundImage:
-              "url(https://images.unsplash.com/photo-1521903062400-b80f2cb8cb9d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80)",
-          }}
-        ></Box>
+          align="center"
+          justify="center"
+        >
+          {/* <Image marginTop={"-30px"} w="100%" src={logoPng} /> */}
+          <Spline scene="https://prod.spline.design/iwVKILINxN7gmP83/scene.splinecode" />
+
+        </Flex>
 
         <Box
           w={{ base: 56, md: 44 }}
           bg="white"
           _dark={{ bg: "gray.800" }}
-          mt={-10}
+          mt={0}
           shadow="lg"
           rounded="lg"
           overflow="hidden"
@@ -44,31 +45,26 @@ const Feature=()=>{
           <chakra.h3
             py={2}
             textAlign="center"
-            fontWeight="bold"
+            fontWeight="500"
             textTransform="uppercase"
             color="gray.800"
             _dark={{ color: "white" }}
             letterSpacing={1}
+            fontSize="13px"
           >
-            Nike Revolt
+            By SumanGiri
           </chakra.h3>
 
           <Flex
             alignItems="center"
-            justifyContent="space-between"
+            justifyContent="center"
             // border="1px solid red"
             py={2}
             px={3}
             bg="gray.400"
             _dark={{ bg: "gray.700" }}
           >
-            <chakra.span
-              fontWeight="bold"
-              color="gray.800"
-              _dark={{ color: "gray.200" }}
-            >
-              $129
-            </chakra.span>
+            <a href="http://sumankumargiri.com/" target="_blank" rel="noreferrer">
             <chakra.button
               bg="gray.800"
               fontSize="xs"
@@ -88,8 +84,8 @@ const Feature=()=>{
                 outline: "none",
               }}
             >
-              Add to cart
-            </chakra.button>
+              Explore
+            </chakra.button></a>
           </Flex>
         </Box>
       </Flex>
---
+++ superFam-frontend/src/components/SignupComponents/spline.js
@@ -0,0 +1,3 @@
+import Spline from '@splinetool/react-spline';
+
+      <Spline scene="https://prod.spline.design/GS8eIr1xAh29Gwdc/scene.splinecode"  />
