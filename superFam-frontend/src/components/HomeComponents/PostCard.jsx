9505ba57fc7b6ac2355c8469652eb9897d245bda
SumanJK <96579301+SumanJK@users.noreply.github.com>
2022-07-20 02:09:11 +0530

    updated post

--- superFam-frontend/src/components/HomeComponents/PostCard.jsx
+++ superFam-frontend/src/components/HomeComponents/PostCard.jsx
@@ -2,55 +2,60 @@ import {
   Box,
   Center,
   useColorModeValue,
-  Heading,
   Text,
   Stack,
   Image,
   Flex,
 } from "@chakra-ui/react";
+import React, { useState } from "react";
+import Heart from "react-animated-heart";
 
 export default function PostCard() {
+
+  const[ isClick, setClick]= useState(false)
   return (
     <Center py={12}>
       <Box
         role={"group"}
-        p={6}
+        p={6 }
+        py="3"
         maxW={"330px"}
         w={"full"}
         bg={useColorModeValue("white", "gray.800")}
         boxShadow=" rgba(136, 165, 191, 0.48) 4px 2px 16px 0px, rgba(189, 195, 220, 0.646) -4px -0px 16px 6px"
         rounded={"lg"}
         pos={"relative"}
-        border={"1px solid red"}
+        // border={"1px solid red"}
         zIndex={1}
       >
         <Flex
-          h="4rem"
-          w="60"
+          h="3rem"
+          mb="1"
+          py="6"
+          w="66"
           overflow="hidden"
-          align="start"
+          align="center"
           justify="end"
           // border={"1px solid red"}
-          my={1}
-          top="0"
           zIndex={1}
         >
           <Box  w="2.4rem"
-              h="2.4rem">
+              h="2.4rem" mr="2">
             <Image
               mr="10px"
               w="2.4rem"
               h="2.4rem"
-              boxShadow="rgba(136, 165, 191, 0.48) 6px 2px 16px 0px, rgba(255, 255, 255, 0.8) -6px -2px 16px 0px"
+              boxShadow="rgba(136, 165, 191, 0.48) 2px 2px 6px 0px, rgba(255, 255, 255, 0.8) -2px -2px 6px 0px"
               borderRadius="50%"
-              src="https://images.unsplash.com/photo-1539125530496-3ca408f9c2d9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+              src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
             />
           </Box>
           <Text
-            color={"gray.500"}
+            color={"gray.600"}
             fontSize={"13px"}
             fontWeight={"600"}
             noOfLines={1}
+            className="userPostFont"
           >
             Sudhir Chavan fsfsfsfafsdfasdfdsffsfsfsfs
           </Text>
@@ -78,7 +83,7 @@ export default function PostCard() {
           }}
         >
           <Image
-            // rounded={'lg'}
+            rounded={'lg'}
             height={230}
             width={282}
             objectFit={"cover"}
@@ -86,9 +91,8 @@ export default function PostCard() {
           />
         </Box>
         <Stack pt={10}>
-          <Heading fontSize={"2xl"} fontFamily={"body"} fontWeight={500}>
-            Nice Chair, pink
-          </Heading>
+        {/* <Heart  isClick={isClick} onClick={() => setClick(!isClick)} /> */}
+        <lottie-player id="toggleLottie" src="https://assets3.lottiefiles.com/datafiles/RCNn6rnkYkbg0RI/data.json"></lottie-player>
           <Stack direction={"row"} align={"center"}>
             <Text fontWeight={800} fontSize={"xl"}>
               $57
