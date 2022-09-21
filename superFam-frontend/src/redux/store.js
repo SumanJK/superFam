859057a77f1aee69785dd28d80ec2486e80b7ddf
SumanJK <girisuman289@gmail.com>
2022-09-21 17:03:53 +0530

    completed project

--- superFam-frontend/src/redux/store.js
+++ superFam-frontend/src/redux/store.js
@@ -1,7 +1,7 @@
 import {legacy_createStore, combineReducers,applyMiddleware} from "redux"
 import thunk from "redux-thunk";
-import { VideoReducer } from "./video/reducer";
 
+import { VideoReducer } from "./video/reducer";
 const { AuthReducer } = require("./auth/reducers");
 const { PostReducer } = require("./post/reducer");
 
